b22079d452ebeb8d1e8f9acbe8125c3acf0837c3
yesersin <yesersin@gmail.com>
2021-12-20 16:25:13 +0300

    first bold

---
+++ dbSql.php
@@ -0,0 +1,32 @@
+<?php
+ 
+class dbSql {
+
+    public $db;
+
+    public function __construct() {
+        $this->db = new \MySQLi("localhost", "root", "", "ilpen") or die(mysqli_error());
+        mysqli_set_charset($this->db, 'utf8');
+    }
+
+    public function mysql() {
+        return $this->db;
+    }
+ 
+
+    public function sonuc($veriler, $single) {
+        $sonuc = "";
+        if ($single) { //1:tekli 
+            while ($row = $veriler->fetch_array(MYSQLI_ASSOC)) {
+                $sonuc = $row;
+            }
+        } else { //0: çoklu
+            $sonuc = array();
+            while ($row = $veriler->fetch_array(MYSQLI_ASSOC)) {
+                $sonuc[] = $row;
+            }
+        }
+        return $sonuc;
+    }
+
+}
