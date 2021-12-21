c4d0bce1d8908ff53e1f88cca1168c081aaa7cdf
yesersin <yesersin@gmail.com>
2021-12-21 23:08:43 +0300

    stok fix

--- setStock.php
+++ setStock.php
@@ -11,14 +11,15 @@ $curl = new curl();
 $ids= ($db->getProductAll());
 
 $count=0;
-foreach ($ids as $val) {
-    $url="https://webservice.ilpen.com.tr/stock_service/json_get_public_stock_by_product_code?type=group&productCode=".$val;
+foreach ($ids as $val) {  
+    $url="https://webservice.ilpen.com.tr/stock_service/json_get_public_stock_by_product_code?type=group&productCode=".$val;  
     $product = $curl->get($url); 
-    if(!is_null($product) || $product!=0){
-        $db->setStock($val, $product["STOCK_AMOUNT"]); //$val=productCode
+    
+    if(!is_null($product) || $product!=0){ 
+        $db->setStock($val, $product[0]["STOCK_AMOUNT"]); //$val=productCode
         $count++;
     }
-}
-echo $count;
+ 
+} 
 
 ?>
